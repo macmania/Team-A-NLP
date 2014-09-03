4ada2ff87b0a105f1477fcdd1e2b8ac0eb488386
jojofabe <jojofabe@vt.edu>
2014-09-03 16:39:02 -0400

    added some stuff

--- U1_FreqDistCompare.py
+++ U1_FreqDistCompare.py
@@ -1,30 +1,53 @@
 from __future__ import division
+from nltk.probability import FreqDist
+from nltk import FreqDist
 import nltk
 from nltk.book import *
 #from nltk.book import text1 - find a way to do this
+'''
 import dateutil
 import pyparsing
 import numpy
 import six
 import matplotlib
-
+'''
 listOfTexts= [text1,text2,text3,text4,text5,text6]
-fdist1= FreqDist(text7)
-commonWords=fdist1.most_common(100) #????
+fdist1= nltk.FreqDist(text7)
+commonWords=fdist1.items() #????
 #fdist1.plot(50, cumulative=False)
 indicativeWords= [word[0] for word in commonWords if word[0].isalpha()]
 #if len(word[0])>4 ]
 count= 0
 masterDict = {}
-for text in listOftext:
+for text in listOfTexts:
     fdist = FreqDist(text)
-    count= count+1
-    print(count)
+    count = count+1
+    #print(count)
     for word in fdist:
         if word in masterDict.keys():
             masterDict[word] = masterDict[word] + fdist[word]
         else:
             masterDict[word] =fdist[word]
+
+sortedDict = dict(sorted(masterDict.items(), key=lambda x: x[1]))
+
+'''for herp in sortedDict:
+    print(herp)'''
+
+
+print('herp\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+
+text7Dist = FreqDist(text7)
+commonWords = text7Dist.items()
+print type(sortedDict)
+
+for common in commonWords:
+    if (common[0] in sortedDict and sortedDict(common[0]) < 1500):
+        print common[0]
+        
+        
+
+#print(sorted(masterDict.items(), key=lambda x: x[1]))
  
 print('\nCollocations')
 colloc= text1.collocations();
@@ -49,4 +72,3 @@ print (len(wordPercentList))
 print('\nPercentage list of all indicative words:\n')
 for i in range(0,len(indicativeWords)-1):
     print(indicativeWords[i],wordPercentList[i])
-      
