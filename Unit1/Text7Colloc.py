d4d61804b26fe5917d9b091ea43ea672220873a9
jojofabe <jojofabe@vt.edu>
2014-09-05 19:19:41 -0400

    moved the files to the appropriate region

--- Unit1/Text7Colloc.py
+++ Unit1/Text7Colloc.py
@@ -13,7 +13,7 @@ import errno
 #starts parsing the text file
 
 tokens = nltk.wordpunct_tokenize(str(text7))
-tokens = [t.lower() for t in tokens if len(t) > 3]
+#tokens = [t.lower() for t in tokens]
 pairs = nltk.bigrams(tokens)
 texasAllPairs = [] 
 
@@ -22,7 +22,7 @@ texasAllPairs = []
 #1 Overview of using collocations
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 trigram_measures = nltk.collocations.TrigramAssocMeasures()
-finder = nltk.collocations.BigramCollocationFinder.from_words(pairs)
+finder = nltk.collocations.BigramCollocationFinder.from_words(text7)
 f2 = nltk.collocations.BigramCollocationFinder.from_words(pairs)
 for i in range(2, 6): 
 	f2.apply_freq_filter(i)
@@ -33,29 +33,14 @@ scored = finder.score_ngrams(bigram_measures.raw_freq)
 word_fd = nltk.FreqDist(tokens)
 bigram_fd = nltk.FreqDist(nltk.bigrams(tokens))
 finder = BigramCollocationFinder(word_fd, bigram_fd)
-print sorted(finder.nbest(trigram_measures.raw_freq, 3)) # sorted(bigram for bigram, score in scored)
+print sorted(finder.nbest(bigram_measures.raw_freq, 10)) # sorted(bigram for bigram, score in scored)
 exclude = set(string.punctuation)
 
 fdist1 = FreqDist(tokens)
 
 finder = TrigramCollocationFinder.from_words(tokens)
-print sorted(finder.nbest(trigram_measures.raw_freq, 4))
+#print sorted(finder.nbest(trigram_measures.raw_freq, 4))
 
 
-#Texas folder collocation start
-files = glob.glob("Texas Fertilizer Plant Explosion/*.txt")
-for name in files: 
-	try: 
-		with open(name) as f: 
-			tok = nltk.bigrams(f.read())
-			tok = [t.lower() for t in tok if len(t) > 3]
-			pair = nltk.bigrams(tok)
-			finder = TrigramCollocationFinder.from_words(tokens)
-			texasAllPairs.append(sorted(finder.nbest(trigram_measures.raw_freq, 4)))
-	except IOError as exc: 
-		if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
-			raise # Propagate other kinds of IOError.
-
-print texasAllPairs
 
 
