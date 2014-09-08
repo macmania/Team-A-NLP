c26aedbcf1ba1eba98598693b6d031268428e804
jojofabe <jojofabe@vt.edu>
2014-09-08 10:47:29 -0400

    added more stuff

--- Unit2/ClassEvent.py
+++ Unit2/ClassEvent.py
@@ -3,7 +3,15 @@
 '''
 
 from nltk.corpus import PlaintextCorpusReader
+from nltk.corpus import brown
+import nltk
 
 corpus_root = 'Islip13Rain'
 worldlists = PlaintextCorpusReader(corpus_root, '.*')
+
 print worldlists.fileids()
+print len(worldlists.sents())
+
+cfd = nltk.ConditionalFreqDist((genre, word) for genre in brown.categories() 
+	for word in brown.words(categories=genre))
+print cfd
