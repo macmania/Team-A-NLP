183cb691102e64c0d7b2247256949089ce394e0a
svsharma <sauravsharma@Sauravs-MacBook-Air.local>
2014-09-11 16:45:03 -0400

    Merge branch 'master' of https://github.com/macmania/UITeamA
    Conflicts:
    	Islip13Rain/10.txt
    	Islip13Rain/2.txt
    	Islip13Rain/3.txt
    	Islip13Rain/4.txt
    	Islip13Rain/8.txt
    	U1_1.py
    	Unit1/Islip13Rain/10.txt
    	Unit1/Islip13Rain/10.txt~FETCH_HEAD
    	Unit1/Islip13Rain/2.txt
    	Unit1/Islip13Rain/2.txt~FETCH_HEAD
    	Unit1/Islip13Rain/3.txt
    	Unit1/Islip13Rain/3.txt~FETCH_HEAD
    	Unit1/Islip13Rain/4.txt
    	Unit1/Islip13Rain/4.txt~FETCH_HEAD
    	Unit1/Islip13Rain/8.txt
    	Unit1/Islip13Rain/8.txt~FETCH_HEAD

--- Unit1/U1_1.py
+++ Unit1/U1_1.py
@@ -7,8 +7,32 @@ from nltk.book import *
 from nltk.stem.porter import *
 
 
+print('Starting creation of stop words')
 
-listOfTexts= [text1,text2,text3,text4,text5,text6,text8,text9]
+stopWords = []
+mitStopWordsFile = open('mit_stop_words.txt')
+stopWords = [line.strip() for line in mitStopWordsFile]
+mitStopWordsFile.close()
+
+print('Done creating stop words')
+
+
+#Populate the list of texts to be used
+listOfTexts = []
+files = glob.glob("TexasExtractedFiles/*.txt")
+count = 0
+for f in files:
+	raw = f.read()
+	#TOKENIZE??????????????????
+	tokens = nltk.word_tokenize(raw)
+	text = nltk.Text(tokens)
+	listOfTexts.append(text)
+	count = count + 1
+	if (count % 50 == 0)
+		print(count)
+
+
+
 
 
 stemmer = PorterStemmer()
@@ -21,17 +45,20 @@ for text in listOfTexts:
     for word in processedWords:
         processedListOfTexts.append(word)
 
+print('Done stemming and stuff')
 
 setOfProcessedWords = set(processedListOfTexts)
 masterDist = FreqDist(processedListOfTexts)
+'''
 stopWords = []
 for key in masterDist.keys():
     if masterDist[key] > 750:
         stopWords.append(key)
+'''
 
-for word in fdist7.most_common(75):
+
+#Use items() because most_common() doesn't work on mac
+for word in fdist7.items()[:75]:
     if word[0] not in stopWords:
         print(word[0])
 
-print('\nCollocations')
-colloc = text7.collocations();
--- Unit2/FrequencyAnalysis.py
+++ Unit2/FrequencyAnalysis.py
@@ -3,8 +3,9 @@ from nltk.probability import FreqDist
 from nltk import FreqDist
 import nltk
 import operator
-from nltk.book import *
+#from nltk.book import *
 from nltk.stem.porter import *
+import glob
 
 
 print('Starting creation of stop words')
@@ -19,26 +20,40 @@ print('Done creating stop words')
 
 #Populate the list of texts to be used
 listOfTexts = []
-files = glob.glob("TexasExtractedFiles/*.txt")
-count = 0
-for f in files:
-	raw = f.read()
-	#TOKENIZE??????????????????
-	tokens = nltk.word_tokenize(raw)
-	text = nltk.Text(tokens)
-	listOfTexts.append(text)
-	count = count + 1
-	if (count % 50 == 0)
-		print(count)
+directory = "TexasExtractedFiles/*.txt"
+files = glob.glob(directory)
+print('There were ')
+print(len(files))
+print('files found in the ' + directory + ' directory.')
+
+#count = 0
+for curr in files:
+#	count = count + 1
+#	if (count % 50) == 0:
+#		print(count)
+
+	try:
+		with open(curr) as f:
+			raw = f.read()
+			#TOKENIZE??????????????????
+			tokens = nltk.word_tokenize(raw)
+			text = nltk.Text(tokens)
+			listOfTexts.append(text)
+
+	except IOError as e:
+		if e.errno != errno.EISDIR:
+			raise
 
 
+print('listOfTexts has: ')
+print(len(listOfTexts))
+print('texts in it')
 
 
+#Process texts
+print('starting stemming and processing')
 
 stemmer = PorterStemmer()
-processedWords7 = [(stemmer.stem(word).lower()) for word in text7 if word.isalpha()]
-fdist7 = FreqDist(processedWords7)
-
 processedListOfTexts = []
 for text in listOfTexts:
     processedWords = [(stemmer.stem(word)).lower() for word in text if word.isalpha()]
@@ -47,18 +62,11 @@ for text in listOfTexts:
 
 print('Done stemming and stuff')
 
-setOfProcessedWords = set(processedListOfTexts)
-masterDist = FreqDist(processedListOfTexts)
-'''
-stopWords = []
-for key in masterDist.keys():
-    if masterDist[key] > 750:
-        stopWords.append(key)
-'''
 
 
+fullDist = FreqDist(processedListOfTexts)
 #Use items() because most_common() doesn't work on mac
-for word in fdist7.items()[:75]:
-    if word[0] not in stopWords:
-        print(word[0])
+for word in fullDist.items()[:75]:
+    #if word[0] not in stopWords:
+    print(word[0])
 
