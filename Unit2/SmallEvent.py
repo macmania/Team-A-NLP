e74fc933b09f8ca89c237c95958778653b198a6d
macmania <jojofabe@vt.edu>
2014-09-13 02:32:37 -0400

    changed some stuff

--- Unit2/SmallEvent.py
+++ Unit2/SmallEvent.py
@@ -8,17 +8,11 @@ import nltk
 from nltk import FreqDist
 from nltk.collocations import *
 import operator
-<<<<<<< HEAD
-=======
 from array import array
 import string 
 
 def is_ascii(s):
-	'''if s == '\'': 
-		return False '''
-
 	return all(ord(c) < 128 for c in s)
->>>>>>> FETCH_HEAD
 
 #analyzes the text based on the number of lines in comparison to the words of a text file
 #if there are more lines than text then it's not an article. (Interesting to look at whether a
@@ -55,21 +49,9 @@ def extractTxtArticle(strTxt):
 	extractTxt = []
 	for r in range(len(strTxt)):
 		if len(strTxt[r].strip(' ')) >= 80 and '|' not in strTxt[r] and '#' not in strTxt[r]:
-<<<<<<< HEAD
 			extractTxt.append(strTxt[r])
-
-	#print extractTxt
 	return extractTxt
 
-
-
-=======
-			strTxt[r] = [s for s in strTxt[r].strip(' ') if is_ascii(s)]
-			extractTxt.append("".join(strTxt[r]).replace("\t", ""))
-
-	return extractTxt
-
->>>>>>> FETCH_HEAD
 #Texas folder collocation start
 files = glob.glob("Texas Fertilizer Plant Explosion/*.txt")
 artcle, nonArtcl = 0, 0
@@ -84,32 +66,14 @@ for name in files:
 				artcle += 1
 				art.append(name)
 				extractTxt = extractTxtArticle(lines)
-<<<<<<< HEAD
-				print name[33:]
-				newFile = open(str("TexasExtractedFiles/" + name[33:]), 'w')
-				newFile.write(str(extractTxt))
-				numArticles += 1
-				#print extractTxt
-=======
 				newFile = open(str("TexasExtractedFiles/" + name[33:]), 'w')
 				newFile.write(str(extractTxt))
 				#numArticles += 1, counts how many articles it has classified successfully
->>>>>>> FETCH_HEAD
-	
 			else: 
 				extractTxt = lines.split()
 				nonArtcl += 1
 				nonArt.append(name)
-<<<<<<< HEAD
 
 	except IOError as exc: 
 		if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
-			raise # Propagate other kinds of IOError.
-
-print numArticles
-=======
-		extractTxt = ' '
-	except IOError as exc: 
-		if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
-			raise # Propagate other kinds of IOError.
->>>>>>> FETCH_HEAD
+			raise # Propagate other kinds of IOError
