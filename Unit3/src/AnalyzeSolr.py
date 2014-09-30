8cd4db0e3b7147d8c0fdc3a5d9df959cfacef102
jojofabe <jojofabe@vt.edu>
2014-09-29 20:48:39 -0400

    added the Solr code and results

--- Unit3/src/AnalyzeSolr.py
+++ Unit3/src/AnalyzeSolr.py
@@ -2,21 +2,31 @@ import json
 from pprint import pprint
 from TaggingCorpus import extractTopPOS
 
-json_data=open('../output/SolrOutput')
-fOutput =  open('../output/SolrContentOutput.txt', 'r+')
+
+json_data=open('../output/SolrFolder/SolrOutput')
+fOutput =  open('../output/SolrFolder/SolrContentOutput.txt', 'r+')
 data = json.load(json_data)
 
 listLength = len(data["response"]["docs"])
-content = ""
+content = " "
+i = 0
+'''
 for query in data["response"]["docs"]:
-    content +=str(query["content"])
-
-fOutput.write(content)
+    fOutput =  open(str( '../output/' + str(i) +'.txt'), 'w')
+    print query["content"]
+    content = query["content"]
+    if len(content) != 0:
+        fOutput.write(str(content))
+    fOutput.close()
+    i += 1
+    #content = content + str(query["content"])
+print ''.join(content)
+'''
+#fOutput.write(''.join(content))
 
 
 
 #pprint(data)
 json_data.close()
-fOutput.close()
 
-extractTopPOS('../output/SolrFolder')
+extractTopPOS('../output/', 'solr folder')
--- Unit3/src/TaggingCorpus.py
+++ Unit3/src/TaggingCorpus.py
@@ -78,17 +78,24 @@ def compareSolr(wordsPOS):
     results, solrWords = " some results ", []
     return results
 
-def extractTopPOS():
+def extractTopPOS(location, descriptionLocation):
+  print location
+  CEcorpus_root = location
+  #Solrcorpus_root = '../output/SolrFolder/'
 
-  CEcorpus_root = '../../lib/Corpus/ClassEvent'
-  Solrcorpus_root = '../output/SolrFolder/'
-  SolrcorpusWord =  PlaintextCorpusReader(Solrcorpus_root, "*\.txt")
   CEWordLists = PlaintextCorpusReader(CEcorpus_root, ".*\.txt")
   CE = CEWordLists.raw() # read all texts, CE = string of all texts
+
+  #SolrWordLists =  PlaintextCorpusReader(Solrcorpus_root, "*\.txt")
+  #SOLR = SolrWordLists.raw()
+
+
   extractTxt = extractTxtArticle(CE) #list of lines separated by \n
+  print "extracting txt", extractTxt
   CE = " ".join(extractTxt) # CE now is a string with all lines of all texts
   sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
   CEsents = sent_tokenizer.tokenize(CE) # CEsents= list of sentences(strings)
+
 
   listOfAllTags = getOurTagCorpus(CEsents) # list of tuples (word, tag)
   
@@ -101,18 +108,18 @@ def extractTopPOS():
   allVerbs = getVerbs(listOfAllTags) # allNouns= list of all nouns 
   topVerbs = getPOS(allVerbs) #posSum= list of top 10 stop-word free nouns
 
-  print "These are the list of top nouns in the class event corpus\n", len(posSum)
+  print "These are the list of top nouns in the ", descriptionLocation, '\n', len(posSum)
   print 'Printing top (stop-word free) nouns '
   for x in posSum:
     print x
 
-  print "These are the list of top nouns EXCEPT NN-NC in the class event corpus\n", len(topNounsExceptNN_NC)
+  print "These are the list of top nouns EXCEPT NN-NC in the", descriptionLocation,  "\n", len(topNounsExceptNN_NC)
   for x in topNounsExceptNN_NC:
     print x
 
 
-  print "These are the list of top verbs in the class event corpus\n", len(topVerbs)
+  print "These are the list of top verbs in the ", descriptionLocation,  "Corpus \n", len(topVerbs)
   for x in topVerbs:
     print x
 
-extractTopPOS()
+#extractTopPOS('../../lib/Corpus/ClassEvent', 'class event')
