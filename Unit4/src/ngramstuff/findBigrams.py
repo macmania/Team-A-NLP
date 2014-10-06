d6e22d686408e31470c0a3744e039ab6e24b1b16
svsharma <sauravsharma@Sauravs-MacBook-Air.local>
2014-10-06 17:08:42 -0400

    modified words

--- Unit4/src/ngramstuff/findBigrams.py
+++ Unit4/src/ngramstuff/findBigrams.py
@@ -3,7 +3,8 @@ f = open('Bigrams10k.txt')
 listOfSentences = f.readlines()
 f.close()
 
-ourWords = ['people', 'fertilizer', 'plant', 'explosion', 'texas', 'west', 'april', 'boston', 'news', 'find', 'fire', 'time', 'point', 'volunteer', 'blast', 'state', 'town', 'community', 'marathon', 'suspect']
+#ourWords = ['people', 'fertilizer', 'plant', 'explosion', 'texas', 'west', 'april', 'boston', 'news', 'find', 'fire', 'time', 'point', 'volunteer', 'blast', 'state', 'town', 'community', 'marathon', 'suspect']
+ourWords = ['fertilizer', 'plant', 'explosion', 'texas', 'boston', 'fire', b'blast']
 for line in listOfSentences:
 	for word in ourWords:
 		if word in line:
