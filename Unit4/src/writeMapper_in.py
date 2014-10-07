d5d8ffe7faed8f8f9cced822f57c5d9165b82d39
Arjun Chandrasekaran <carjun@vt.edu>
2014-10-06 23:42:01 -0400

    updated wwriteMapper_in.py

--- Unit4/src/writeMapper_in.py
+++ Unit4/src/writeMapper_in.py
@@ -4,7 +4,7 @@ def writeTrainList():
     files = glob.glob('../../Unit2/output/TexasExtractedFiles/*.txt')
     f= open('mapper_in.txt','w')
     for name in files:
-        f.write(name+'\n')
+        f.write('../../Unit2/output/TexasExtractedFiles/'+os.path.basename(name)+'\n')
     f.close()            
     
 writeTrainList()
